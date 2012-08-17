0fb9e7784fdcac6462653c8536cb6df658ea76f3
kongflower <shindoohwa@gmail.com>
2012-08-17 12:09:14 +0900

    commit controller

--- plants/app/controller/searchController.js
+++ plants/app/controller/searchController.js
@@ -2,11 +2,20 @@ Ext.define('plants.controller.searchController', {
     extend: 'Ext.app.Controller',
     config: {
     	refs: {
-            
+    		mainView: '#mainView',
+    		searchPlants: '#searchPlants',
         },
         control: {
-        	
+        	"[action=movePage]": {
+                tap: 'onMovePage'
+            },
         }
+    },
+    
+    onMovePage : function(button, e, options){
+    	this.getMainView().push(button.value);
+    	//this.getMainView().animateActiveItem(button.value, { type: "slide", direction: "left" });
+    	console.log('plants');
     }
 
 });
