2cd16e0aafa4be39b23429f42aa33ffd7f7bd0e6
kongflower <shindoohwa@gmail.com>
2012-09-04 19:08:03 +0900

    test

--- plants/app/view/test.js
+++ plants/app/view/test.js
@@ -10,16 +10,16 @@ Ext.define("plants.view.test", {
     	title: 'test',
     	
         items: [
-//            {
-//            	xtype    : 'video',
-//            	x        : 600,
-//            	y        : 300,
-//            	width    : 175,
-//            	height   : 98,
-//            	url      : "http://www.youtube.com/watch?feature=player_detailpage&v=fHjrffB9BxI",
-//            }
+            {
+            	xtype    : 'video',
+            	x        : 600,
+            	y        : 300,
+            	width    : 175,
+            	height   : 98,
+            	url      : "http://192.168.0.192:9090/plants/resources/images/space.mp4",
+            }
         ],
-        html: '<iframe title="YouTube video player" width="480" height="390" src="http://192.168.0.192:9090/plants/resources/images/space.mp4" frameborder="0" allowfullscreen></iframe>'
+//        html: '<iframe title="YouTube video player" width="480" height="390" src="http://192.168.0.192:9090/plants/resources/images/space.mp4" frameborder="0" allowfullscreen></iframe>'
 
     }
 });
